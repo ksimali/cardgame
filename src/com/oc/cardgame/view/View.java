7e61f1ca9b2072482f953c021cdc8c727310cfa7
ksimali <cassim.ali@outlook.fr>
2025-01-06 23:20:21 -0500

    Setup View implementation into Console

--- src/com/oc/cardgame/view/View.java
+++ src/com/oc/cardgame/view/View.java
@@ -1,39 +1,55 @@
 package com.oc.cardgame.view;
 
+import java.util.Scanner;
+
 import com.oc.cardgame.controller.GameController;
 
 public class View {
-
-	public void something() {}
 	
-	public void setController(GameController gc) {}
+	GameController controller;
+	Scanner keyboard = new Scanner(System.in);
 	
+	public void setController(GameController gc) {
+		this.controller = gc;
+	}
+	
+	// Method pour ajouter des joueurs
 	public void promptForPlayerName() {
-		
+		System.out.println("Entrer le nom du joueur:");
+		String name = keyboard.nextLine();
+		if(name.isEmpty()) {
+			controller.startGame();
+		}else {
+			controller.addPlayer(name);
+		}
 	};
 	
 	public void promptForFlip() {
-		
+		System.out.println("Appuyer sur Entrer pour montrer les cartes");
+		keyboard.nextLine();
+		controller.flipCards();
 	};
 	
 	public void promptForNewGame() {
-		
+		System.out.println("Appuyer sur Entrer pour rejouer");
+		keyboard.nextLine();
+		controller.startGame();
 	};
 	
-	public void showWinner(String name) {
-		
+	public void showWinner(String playerName) {
+		System.out.println("Le gagnant est " + playerName + " !");
 	}
 	
-	public void showPlayerName(int size, String playerName) {
-		
+	public void showPlayerName(int playerIndex, String playerName) {
+		System.out.println("[" +playerIndex+"]["+playerName+"]");
 	};
 	
-	public void showFaceDownCardForPlayer(int i, String playerName) {
-		
+	public void showFaceDownCardForPlayer(int playerIndex, String playerName) {
+		System.out.println("[" +playerIndex+"]["+playerName+"][x][x]");
 	};
 	
-	public void showCardForPlayer(int i, String name,
+	public void showCardForPlayer(int playerIndex, String playerName,
 			String rank, String suit) {
-		
+		System.out.println("["+playerIndex+"]["+playerName+"]["+rank+"]["+suit+"]");
 	};
 }
