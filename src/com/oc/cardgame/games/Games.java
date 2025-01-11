1eb7254dbe3bc4f80ad2442ed309285532627066
ksimali <cassim.ali@outlook.fr>
2025-01-10 23:31:56 -0500

    Setup GameSwingView feature :
    - Create GameSwingView.java Class & Add JFrame, JTextArea and Methods
    - call GameSwingView in Games Class's main method

--- src/com/oc/cardgame/games/Games.java
+++ src/com/oc/cardgame/games/Games.java
@@ -3,11 +3,17 @@ package com.oc.cardgame.games;
 import com.oc.cardgame.controller.GameController;
 import com.oc.cardgame.model.Deck;
 import com.oc.cardgame.view.CommandLineView;
+import com.oc.cardgame.view.GameSwingView;
 
 public class Games {
 	
 	public static void main(String[] args) {
-		GameController gc = new GameController(new Deck(), new CommandLineView(),  new HighCardGameEvaluator());
+		GameSwingView gsv = new GameSwingView(); // instance de gameSwingView qu'on va passer en paramètre dans le controller
+		gsv.createAndShowGUI();
+		
+		GameController gc = new GameController(new Deck(), gsv,  new HighCardGameEvaluator());
+		
+		//GameController gc = new GameController(new Deck(), new CommandLineView(),  new HighCardGameEvaluator());
 		//GameController gc = new GameController(new Deck(), new View(),  new LowCardGameEvaluator());
 		gc.run();
 	}
